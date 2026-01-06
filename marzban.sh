0f1d8bc8ee689a9ae10e17229f490328d6e463f8
13McDuck13 <157541121+13McDuck13@users.noreply.github.com>
2026-01-06 15:18:13 +0300

    Create marzban.sh

---
+++ marzban.sh
@@ -0,0 +1,33 @@
+apt-get update; apt-get upgrade -y; apt-get install curl socat git -y 
+
+curl -fsSL https://get.docker.com | sh
+
+mkdir /var/lib/marzban-node
+mkdir /var/lib/marzban
+mkdir ~/marzban-node
+
+cd ~/marzban-node
+
+echo "
+services:
+  marzban-node:
+    image: gozargah/marzban-node:latest
+    restart: always
+    network_mode: host
+
+    environment:
+      SSL_CLIENT_CERT_FILE: "/var/lib/marzban-node/ssl_client_cert.pem"
+      SERVICE_PROTOCOL: "rest"
+      SERVICE_PORT: "62050"
+      XRAY_API_PORT: "62051"
+      DEBUG: "false"
+
+    volumes:
+      - /var/lib/marzban-node:/var/lib/marzban-node
+      - /var/lib/marzban:/var/lib/marzban
+" > docker-compose.yml
+
+
+nano /var/lib/marzban-node/ssl_client_cert.pem
+
+docker compose up -d --force-recreate
